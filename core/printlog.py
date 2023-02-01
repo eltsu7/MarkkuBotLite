a9358946335d6e03b47b3aa8f26a68aa85ce325a
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2023-02-01 14:30:16 +0200

    rename parameter

--- core/printlog.py
+++ core/printlog.py
@@ -2,9 +2,9 @@ from datetime import datetime
 from telegram import Update
 
 
-def printlog(update: Update, message_type):
+def printlog(update: Update, message):
     username = update.message.from_user.username
     time_string = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 
-    print(f"{time_string} @{username} {message_type}", flush=True)
+    print(f"{time_string} @{username} {message}", flush=True)
 
